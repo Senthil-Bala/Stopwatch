85cddd6b1b42e291070d6207dfa2aacd303ab877
Senthil-Bala <senthilkumarbala100@gmail.com>
2023-12-22 18:28:22 +0530

    Adjustment made

--- src/Components/Country/CountryCards.jsx
+++ src/Components/Country/CountryCards.jsx
@@ -1,36 +1,19 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 function CountryCards({ cards, searchValue }) {
+  const filteredCountries = cards.filter((country) => {
+    const countryName = country.name.common.toLowerCase();
+    return countryName.includes(searchValue.toLowerCase());
+  });
+
   return (
     <div>
-      <Container>
-        <Row>
-          {cards.filter((ele, index) => {
-            return searchValue.toLowerCase() === ""
-              ? ele
-              : ele.name.common.toLowerCase().includes(searchValue);
-          })
-         .map((ele, index) => (
-            <Col xs={12} sm={12} md={4} lg={4} xl={4}>
-              <Card key={index} className="card">
-                <Card.Img
-                  variant="top"
-                  src={ele.flags.png}
-                  alt={ele.name.common}
-                  className="flag-image"
-                />
-                <Card.Body>
-                  <Card.Title>{ele.name.common}</Card.Title>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      {filteredCountries.map((country, index) => (
+        <div className="image" key={index}>
+          <img src={country.flags.png} alt={country.name.common} />
+          <h2 className="text-white">{country.name.common}</h2>
+        </div>
+      ))}
     </div>
   );
 }
--- src/Components/Country/CountryData.jsx
+++ src/Components/Country/CountryData.jsx
@@ -1,46 +1,53 @@
-import React from "react";
-import "./CountryData.css";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CountryCards from "./CountryCards";
 import "./CountryData.css";
 
 function CountryData() {
   const [countryData, setCountryData] = useState([]);
-  const [loading, setLoading] = useState(true); // Set initial loading state to true
+  const [loading, setLoading] = useState(true);
   const [errorMessage, setErrorMessage] = useState("");
-  const [search, setsearch] = useState('')
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
       .then((res) => {
-        console.log(res.data);
         setCountryData(res.data);
         setErrorMessage("");
       })
       .catch((error) => {
-        console.log(error);
         setErrorMessage(error.message);
       })
       .finally(() => {
-        setLoading(false); // Set loading to false after receiving the response (success or error)
+        setLoading(false);
       });
   }, []);
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
   return (
     <div>
       <h1 className="text-center m-3">Country App</h1>
-      <input id="search" type="text" placeholder="Search Your Country" value={search} onChange={e=>setsearch(e.target.value)}/>
+      <input
+        id="search"
+        type="text"
+        placeholder="Search Your Country"
+        value={search}
+        onChange={handleSearch}
+      />
       <hr />
       {loading ? (
         <h2 className="loading">Loading...</h2>
-      ) : (errorMessage ? (
+      ) : errorMessage ? (
         <h2 className="error">{errorMessage}</h2>
       ) : (
-        <CountryCards cards={countryData} searchValue={search} />)
+        <CountryCards cards={countryData} searchValue={search} />
       )}
     </div>
   );
 }
+
 export default CountryData;
